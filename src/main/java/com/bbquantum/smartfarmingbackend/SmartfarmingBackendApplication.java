b55cd6446109a54bfc1eea5c1c480f453595b5e3
Benney-Walker <benjaminbadubenney@gmail.com>
2026-04-23 21:52:08 +0000

    Init Commit

---
+++ src/main/java/com/bbquantum/smartfarmingbackend/SmartfarmingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bbquantum.smartfarmingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfarmingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartfarmingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbquantum/smartfarmingbackend/SmartfarmingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbquantum.smartfarmingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfarmingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
