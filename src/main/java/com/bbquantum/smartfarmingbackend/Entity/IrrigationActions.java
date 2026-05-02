27dd3b962d851c440b019bbc6a7100635b99a9b5
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-02 09:31:53 +0000

    Added entity to store action details

--- src/main/java/com/bbquantum/smartfarmingbackend/Entity/IrrigationActions.java
+++ src/main/java/com/bbquantum/smartfarmingbackend/Entity/IrrigationActions.java
@@ -11,10 +11,6 @@ public class IrrigationActions {
     @Id
     private String actionId;
 
-    @OneToOne
-    @JoinColumn(name = "ModelDecisions_decisionId")
-    private ModelDecisions decision;
-
     @Column(length = 1000, nullable = false)
     private String actionData;
 
@@ -23,33 +19,68 @@ public class IrrigationActions {
     @Enumerated(EnumType.STRING)
     private ActionStatus actionStatus;
 
+    @Column(length = 1000)
+    private String message;
+
+    @OneToOne(mappedBy = "action")
+    private ModelResponse modelResponse;
+
     public IrrigationActions() {}
 
-    public IrrigationActions(String actionId, ModelDecisions decision, String actionData, LocalDateTime actionTime, ActionStatus actionStatus) {
+    public IrrigationActions(String actionId, String actionData, LocalDateTime actionTime, ActionStatus actionStatus, String message, ModelResponse modelResponse) {
         this.actionId = actionId;
-        this.decision = decision;
         this.actionData = actionData;
         this.actionTime = actionTime;
         this.actionStatus = actionStatus;
+        this.message = message;
+        this.modelResponse = modelResponse;
     }
 
     public String getActionId() {
         return actionId;
     }
 
-    public ModelDecisions getDecision() {
-        return decision;
+    public void setActionId(String actionId) {
+        this.actionId = actionId;
     }
 
     public String getActionData() {
         return actionData;
     }
 
+    public void setActionData(String actionData) {
+        this.actionData = actionData;
+    }
+
     public LocalDateTime getActionTime() {
         return actionTime;
     }
 
+    public void setActionTime(LocalDateTime actionTime) {
+        this.actionTime = actionTime;
+    }
+
     public ActionStatus getActionStatus() {
         return actionStatus;
+    }
+
+    public void setActionStatus(ActionStatus actionStatus) {
+        this.actionStatus = actionStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ModelResponse getModelResponse() {
+        return modelResponse;
+    }
+
+    public void setModelResponse(ModelResponse modelResponse) {
+        this.modelResponse = modelResponse;
     }
 }
