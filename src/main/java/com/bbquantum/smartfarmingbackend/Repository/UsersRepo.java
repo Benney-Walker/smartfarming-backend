3a3dbb7ef3176d89c3c7eea143d18e556afc62f3
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-02 09:30:48 +0000

    Updated Users repo

--- src/main/java/com/bbquantum/smartfarmingbackend/Repository/UsersRepo.java
+++ src/main/java/com/bbquantum/smartfarmingbackend/Repository/UsersRepo.java
@@ -4,6 +4,12 @@ import com.bbquantum.smartfarmingbackend.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsersRepo extends JpaRepository<Users, Integer> {
+
+    Optional<Users> findByEmailAddress(String email);
+
+    Optional<Users> findByUserName(String username);
 }
