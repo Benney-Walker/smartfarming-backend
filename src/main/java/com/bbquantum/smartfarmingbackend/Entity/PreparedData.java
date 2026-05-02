686fc53bafafc6f5ea1232ef8ef10ba3100623f1
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-02 09:09:19 +0000

    Renamed entity

---
+++ src/main/java/com/bbquantum/smartfarmingbackend/Entity/PreparedData.java
@@ -0,0 +1,142 @@
+package com.bbquantum.smartfarmingbackend.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class PreparedData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long preparedDataId;
+
+    private String soilMoisture;
+
+    private String soilTemperature;
+
+    private String humidity;
+
+    private String lightIntensity;
+
+    private String rainFallPossibility;
+
+    private LocalDateTime timeProcessed;
+
+    @OneToOne
+    @JoinColumn
+    private SensorData sensorData;
+
+    @OneToOne
+    @JoinColumn
+    private WeatherData weatherData;
+
+    private boolean isDecisionMade;
+
+    @OneToOne(mappedBy = "preparedData")
+    private ModelResponse modelResponse;
+
+    public PreparedData() {}
+
+    public PreparedData(String soilMoisture, String soilTemperature, String humidity, String lightIntensity,
+                        String rainFallPossibility, LocalDateTime timeProcessed, SensorData sensorData, WeatherData weatherData,
+                        boolean isDecisionMade) {
+        this.soilMoisture = soilMoisture;
+        this.soilTemperature = soilTemperature;
+        this.humidity = humidity;
+        this.lightIntensity = lightIntensity;
+        this.rainFallPossibility = rainFallPossibility;
+        this.timeProcessed = timeProcessed;
+        this.sensorData = sensorData;
+        this.weatherData = weatherData;
+        this.isDecisionMade = isDecisionMade;
+    }
+
+    public long getPreparedDataId() {
+        return preparedDataId;
+    }
+
+    public void setPreparedDataId(long preparedDataId) {
+        this.preparedDataId = preparedDataId;
+    }
+
+    public String getSoilMoisture() {
+        return soilMoisture;
+    }
+
+    public void setSoilMoisture(String soilMoisture) {
+        this.soilMoisture = soilMoisture;
+    }
+
+    public String getSoilTemperature() {
+        return soilTemperature;
+    }
+
+    public void setSoilTemperature(String soilTemperature) {
+        this.soilTemperature = soilTemperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getLightIntensity() {
+        return lightIntensity;
+    }
+
+    public void setLightIntensity(String lightIntensity) {
+        this.lightIntensity = lightIntensity;
+    }
+
+    public String getRainFallPossibility() {
+        return rainFallPossibility;
+    }
+
+    public void setRainFallPossibility(String rainFallPossibility) {
+        this.rainFallPossibility = rainFallPossibility;
+    }
+
+    public LocalDateTime getTimeProcessed() {
+        return timeProcessed;
+    }
+
+    public void setTimeProcessed(LocalDateTime timeProcessed) {
+        this.timeProcessed = timeProcessed;
+    }
+
+    public SensorData getSensorData() {
+        return sensorData;
+    }
+
+    public void setSensorData(SensorData rawData) {
+        this.sensorData = rawData;
+    }
+
+    public WeatherData getWeatherData() {
+        return weatherData;
+    }
+
+    public void setWeatherData(WeatherData weatherData) {
+        this.weatherData = weatherData;
+    }
+
+    public boolean isDecisionMade() {
+        return isDecisionMade;
+    }
+
+    public void setDecisionMade(boolean decisionMade) {
+        isDecisionMade = decisionMade;
+    }
+
+    public ModelResponse getModelResponse() {
+        return modelResponse;
+    }
+
+    public void setModelResponse(ModelResponse modelResponse) {
+        this.modelResponse = modelResponse;
+    }
+}
