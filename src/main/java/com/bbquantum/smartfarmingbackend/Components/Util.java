c2fa63abfd94eede5a5a991170a232c9e3bce82e
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-02 09:06:51 +0000

    Added check model status method template

---
+++ src/main/java/com/bbquantum/smartfarmingbackend/Components/Util.java
@@ -0,0 +1,12 @@
+package com.bbquantum.smartfarmingbackend.Components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Util {
+
+    // Template
+    public String checkModelStatus() {
+        return "ONLINE";
+    }
+}
