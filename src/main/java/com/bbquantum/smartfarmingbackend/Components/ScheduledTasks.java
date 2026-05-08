8ce2f3547998aa9e867f454c2af2f1f1f6a4136d
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-08 07:27:05 +0000

    Added updated ask for irrigation command method

--- src/main/java/com/bbquantum/smartfarmingbackend/Components/ScheduledTasks.java
+++ src/main/java/com/bbquantum/smartfarmingbackend/Components/ScheduledTasks.java
@@ -3,19 +3,17 @@ package com.bbquantum.smartfarmingbackend.Components;
 import com.bbquantum.smartfarmingbackend.Contants.Model;
 import com.bbquantum.smartfarmingbackend.DTO.HybridComDTO.InputResponse;
 import com.bbquantum.smartfarmingbackend.DTO.HybridComDTO.PreparedDataHolder;
+import com.bbquantum.smartfarmingbackend.DTO.WeatherDTO.ReceivedForcast;
 import com.bbquantum.smartfarmingbackend.Entity.ModelResponse;
 import com.bbquantum.smartfarmingbackend.Entity.PreparedData;
 import com.bbquantum.smartfarmingbackend.Entity.SensorData;
 import com.bbquantum.smartfarmingbackend.Entity.WeatherData;
-import com.bbquantum.smartfarmingbackend.Repository.PreparedDataRepo;
-import com.bbquantum.smartfarmingbackend.Repository.SensorDataRepo;
-import com.bbquantum.smartfarmingbackend.Repository.WeatherDataRepo;
-import com.bbquantum.smartfarmingbackend.Service.DataService;
-import com.bbquantum.smartfarmingbackend.Service.DecisionAndActionService;
-import com.bbquantum.smartfarmingbackend.Service.WebSocketService;
+import com.bbquantum.smartfarmingbackend.Service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
@@ -24,64 +22,43 @@ public class ScheduledTasks {
     private DataService dataService;
 
     @Autowired
-    private SensorDataRepo sensorDataRepo;
-
-    @Autowired
-    private WeatherDataRepo weatherDataRepo;
-
-    @Autowired
-    private WebSocketService webSocketService;
-
-    @Autowired
     private Util util;
 
     @Autowired
     private DecisionAndActionService DAService;
 
     @Autowired
-    private PreparedDataRepo preparedDataRepo;
+    private HybridEngineService engineService;
 
-    @Scheduled(fixedRate = 180000) //Runs every 3 minutes
+    @Autowired
+    private WeatherService weatherService;
+
+    @Scheduled(fixedRate = 60000) //Runs every 1 minutes
     public void prepareReceivedData() {
+
         // Unprocessed sensor data
-        SensorData sensorRecord = sensorDataRepo.findByIsProcessed(false).orElse(null);
+        SensorData sensorRecord = dataService.getUnprocessedSensorData();
+        if (sensorRecord == null) {
+            return;
+        }
 
         // Unprocessed weather data
-        WeatherData weatherData = weatherDataRepo.findByIsProcessed(false).orElse(null);
-
-        if (sensorRecord == null || weatherData == null) {
+        WeatherData weatherData = dataService.getRespectiveHourlyForecast(sensorRecord.getTimeOfArrival());
+        if (weatherData == null) {
             return;
         }
 
         dataService.processRawData(sensorRecord, weatherData);
-
-        sensorRecord.setProcessed(true);
-        sensorDataRepo.save(sensorRecord);
-
-        weatherData.setProcessed(true);
-        weatherDataRepo.save(weatherData);
-    }
-
-    @Scheduled(fixedRate = 300000) //Checks model status every 5 minutes
-    public void checkHybridStatus() {
-        webSocketService.sendHybridStatus(util.checkModelStatus());
     }
 
     @Scheduled(fixedRate = 240000) // sends data to model for prediction and decision every 4 minutes
     public void askForIrrigationDecisions() {
-        PreparedData data = preparedDataRepo.findByIsDecisionMade(false).orElse(null);
+        PreparedData data = null;
         if (data == null) return;
 
-        PreparedDataHolder dataHolder = new PreparedDataHolder(
-                data.getPreparedDataId(),
-                Double.parseDouble(data.getSoilMoisture()),
-                Double.parseDouble(data.getSoilTemperature()),
-                Double.parseDouble(data.getHumidity()),
-                Double.parseDouble(data.getLightIntensity()),
-                Double.parseDouble(data.getRainFallPossibility())
-        );
+        PreparedDataHolder dataHolder = dataService.prepareData(data);
 
-        String cloudModelStatus = util.checkModelStatus();
+        String cloudModelStatus = engineService.checkModelStatus();
 
         InputResponse modelResponse;
         if (cloudModelStatus.equals("OFFLINE")) {
