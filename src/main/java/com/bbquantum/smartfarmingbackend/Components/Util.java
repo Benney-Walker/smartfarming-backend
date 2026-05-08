cc8661af5dc3dacc4491bbebb48f94feec19141d
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-08 07:32:55 +0000

    Registered needed classes as beans and method to generate specific entity ids

--- src/main/java/com/bbquantum/smartfarmingbackend/Components/Util.java
+++ src/main/java/com/bbquantum/smartfarmingbackend/Components/Util.java
@@ -1,12 +1,64 @@
 package com.bbquantum.smartfarmingbackend.Components;
 
+import com.bbquantum.smartfarmingbackend.Entity.EntityIdGeneration;
+import com.bbquantum.smartfarmingbackend.Repository.EntityIdGenRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class Util {
 
-    // Template
-    public String checkModelStatus() {
-        return "ONLINE";
+    @Autowired
+    private EntityIdGenRepo entityIdGenRepo;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    public String generateEntityId(String entityName) {
+        String prefix;
+        int code;
+
+        switch (entityName) {
+            case "FIELD" -> {
+                prefix = "FD";
+                code = 100000;
+                return getId(entityName, prefix, code);
+            }
+            case "PREPARED_DATA" -> {
+                prefix = "DT";
+                code = 200000;
+                return getId(entityName, prefix, code);
+            }
+            case "IRRIGATION_ACTION" -> {
+                prefix = "AC";
+                code = 300000;
+                return getId(entityName, prefix, code);
+            }
+        }
+
+        return "";
+    }
+
+    private String getId(String entityName, String prefix, int code) {
+        EntityIdGeneration entity = entityIdGenRepo.findByEntityName(entityName)
+                .orElse(new EntityIdGeneration(entityName, code));
+
+        int oldCode = entity.getEntityCode();
+        int newCode = oldCode + 1;
+
+        entity.setEntityCode(newCode);
+        entityIdGenRepo.save(entity);
+
+        return prefix + newCode;
     }
 }
