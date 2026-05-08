45ca24b46431fdb396a13a62811ffed125727c31
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-08 07:11:09 +0000

    Updated method to talk to cloud model

--- src/main/java/com/bbquantum/smartfarmingbackend/Service/DecisionAndActionService.java
+++ src/main/java/com/bbquantum/smartfarmingbackend/Service/DecisionAndActionService.java
@@ -1,24 +1,53 @@
 package com.bbquantum.smartfarmingbackend.Service;
 
+import com.bbquantum.smartfarmingbackend.Components.Util;
 import com.bbquantum.smartfarmingbackend.Contants.ActionStatus;
 import com.bbquantum.smartfarmingbackend.Contants.ModelDecision;
 import com.bbquantum.smartfarmingbackend.DTO.HybridComDTO.InputResponse;
 import com.bbquantum.smartfarmingbackend.DTO.MQTT.ActionFeedBack;
 import com.bbquantum.smartfarmingbackend.DTO.HybridComDTO.PreparedDataHolder;
+import com.bbquantum.smartfarmingbackend.DTO.MQTT.IrrigationAction;
 import com.bbquantum.smartfarmingbackend.Entity.IrrigationActions;
+import com.bbquantum.smartfarmingbackend.Entity.ModelResponse;
 import com.bbquantum.smartfarmingbackend.Repository.IrrigationActionsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class DecisionAndActionService {
 
-    private final IrrigationActionsRepo irrigationActionsRepo;
+    @Autowired
+    private IrrigationActionsRepo irrigationActionsRepo;
 
-    private final WebSocketService webSocketService;
+    @Autowired
+    private HybridEngineService hybridEngineService;
 
-    public DecisionAndActionService(IrrigationActionsRepo irrigationActionsRepo, WebSocketService webSocketService) {
-        this.irrigationActionsRepo = irrigationActionsRepo;
-        this.webSocketService = webSocketService;
+    @Autowired
+    private MqttService mqttService;
+
+    @Autowired
+    private Util util;
+
+    public InputResponse callCloudModel(PreparedDataHolder data) {
+
+        Map response = hybridEngineService.getIrrigationDecision(data);
+
+        String decision = (String) response.get("decision");
+        int waterQuantity = (int) response.get("waterQuantity");
+        int confidence = (int) response.get("confidence");
+        String decisionReason = (String) response.get("decisionReason");
+        String preparedDataId = (String) response.get("preparedDataId");
+
+        return new InputResponse(
+                decision,
+                waterQuantity,
+                confidence,
+                decisionReason,
+                preparedDataId
+        );
     }
 
     public void updateActionDetails(ActionFeedBack feedBack) {
@@ -35,9 +64,6 @@ public class DecisionAndActionService {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public InputResponse callCloudModel(PreparedDataHolder data) {
 
         return null;
     }
@@ -49,9 +75,5 @@ public class DecisionAndActionService {
 
     private boolean isIrrigationNeeded(String decision) {
         return !decision.equals(ModelDecision.NO_IRRIGATION.name());
-    }
-
-    public void storeModelResponse(InputResponse response) {
-
     }
 }
